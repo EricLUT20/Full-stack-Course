4af787e875c6a9423bd1afbd104da1ee23e58502
EricLUT20 <ericthomasliskii@gmail.com>
2023-12-15 17:23:40 +0200

    Part 9 commit

--- MEAN-Stack/app.js
+++ MEAN-Stack/app.js
@@ -57,6 +57,10 @@ app.get("/", (req, res) => {
   res.send("Invalid")
 })
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"))
+})
+
 app.listen(port, () => {
   console.log("Server started on port " + port)
 })
